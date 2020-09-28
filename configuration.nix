6e29e4442005403899c1e72dc38ac510848983b5
Mason Mackaman <masondeanm@aol.com>
2020-09-28 00:00:35 -0400

    Remove comments

--- configuration.nix
+++ configuration.nix
@@ -91,12 +91,8 @@
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
-  # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
 
   services = {
     picom = {
