ca8f511ad4ecd4ec68e54b5a442930531bc19dbb
Mason Mackaman <masondeanm@aol.com>
2020-12-11 23:34:00 -0500

    Rename signal-desktop nixpkgs

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,7 +3,7 @@
     brightness.url = "github:ursi/brightness";
     json-format.url = "github:ursi/json-format";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-signal.url = "github:NixOS/nixpkgs/22148780509c003bf5288bba093051a50e738ce9";
+    signal-desktop-nixpkgs.url = "github:NixOS/nixpkgs/22148780509c003bf5288bba093051a50e738ce9";
   };
 
   outputs =
@@ -11,7 +11,7 @@
       self, nixpkgs,
       utils,
       brightness, json-format,
-      nixpkgs-signal
+      signal-desktop-nixpkgs
     }:
       let
         system = "x86_64-linux";
@@ -21,7 +21,7 @@
           config = { allowUnfree = true; };
           overlays = [
             (utils.mkFlakePackages system { inherit brightness json-format; })
-            (_: _: { inherit (nixpkgs-signal.legacyPackages.${system}) signal-desktop; })
+            (_: _: { inherit (signal-desktop-nixpkgs.legacyPackages.${system}) signal-desktop; })
           ];
         };
       in
