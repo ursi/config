75c11a1c7b2e2ee803e78f56d3204ed882337580
Mason Mackaman <masondeanm@aol.com>
2021-06-01 22:43:55 -0400

    Add prefetch-nixpkgs

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -159,6 +159,22 @@
                       ];
                   in
                   [ gnome3.nautilus # for seeing images
+                    (writeShellScriptBin
+                       "prefetch-nixpkgs"
+                       ''
+                       url=https://github.com/NixOS/nixpkgs/archive/$1.tar.gz
+                       nix-prefetch-url $url --type sha256 --unpack | {
+                         read hash
+
+                         echo "\
+                       (fetchTarball
+                          { url = \"$url\";
+                            sha256 = \"$hash\";
+                          }
+                       )"
+                       }
+                       ''
+                    )
                     qemu
                     (writeShellScriptBin
                        "snowball"
