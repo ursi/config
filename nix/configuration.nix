3b9191422822a59bb7a4116657c0b13eb3d6bd7b
Mason Mackaman <masondeanm@aol.com>
2021-06-08 15:39:53 -0400

    Remove stderr output

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -163,7 +163,7 @@
                        "prefetch-nixpkgs"
                        ''
                        url=https://github.com/NixOS/nixpkgs/archive/$1.tar.gz
-                       nix-prefetch-url $url --type sha256 --unpack | {
+                       nix-prefetch-url $url --type sha256 --unpack 2> /dev/null | {
                          read hash
 
                          echo "\
