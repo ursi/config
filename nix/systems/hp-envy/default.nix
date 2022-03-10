5eaaf8bc6d982c22a6e35b1080f8d21f22d0857d
Mason Mackaman <masondeanm@aol.com>
2022-03-10 11:10:06 -0500

    Upgrade hp-envy to linux 5.16

--- nix/systems/hp-envy/default.nix
+++ nix/systems/hp-envy/default.nix
@@ -11,8 +11,9 @@ with builtins;
       # 5.13 didn't fix the issue with my usb wifi, and the built in wifi crashes my system
       # 5.14 had the neovim glich with cursorcolumn
       # 5.15.4 doesn't fix the issues I had with 5.13, but the usb wifi problem *might* happen less often. I was using 5.15.0-rc7, for a while with no crashes, but after this started crashing, I went back to it and it still happened. Also my audio occasionally crashes. As I'm typing this I realize that in order to use 5.15.4 I had to update nixpkgs, so mabye that's what caused 5.15.0-rc7 to not work anymore.
+      # 5.16 so far no crashes using the built in wifi card, and no audio crashes
 
-      { kernelPackages = p.linuxKernel.packages.linux_5_15;
+      { kernelPackages = p.linuxKernel.packages.linux_5_16;
 
         loader =
           { systemd-boot.enable = true;
