a824fe7186709876aa703c34094e5757fe99d7d7
Mason Mackaman <masondeanm@aol.com>
2022-06-03 06:53:07 -0400

    Fix secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ with builtins;
 let
   all-keys =
     concatMap
-      (a: attrValues (import (../systems + "/${a}/info.nix")).ssh-keys)
+      (a: attrValues ((import (../systems + "/${a}/info.nix")).ssh-keys or {}))
       (attrNames (readDir ../systems));
 in
 { "netrc.age".publicKeys = all-keys; }
