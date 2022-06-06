4a231fa778db3c55fbd5cf081b8aaa5bb1cee789
Mason Mackaman <masondeanm@aol.com>
2022-06-06 15:58:26 -0400

    Add ssh key for e-ink tablet

--- systems/do-nixos-0/default.nix
+++ systems/do-nixos-0/default.nix
@@ -3,6 +3,7 @@
 
   users.users.mason.openssh.authorizedKeys.keys =
       [ (import ../hp-envy/info.nix).ssh-keys.mason
+        (import ../lumi-2/info.nix).ssh-keys.u0_a137
         (import ../oppo/info.nix).ssh-keys.u0_a334
         (import ../surface-go/info.nix).ssh-keys.mason
       ];
---
+++ systems/lumi-2/info.nix
@@ -0,0 +1,2 @@
+{ ssh-keys.u0_a137 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGDRhqMLdSkm48yEXqh+BwM/7HZ/a3nQt9N+S+K7rQq"; }
+
