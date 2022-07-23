cc9e2843446a7edb1d7b01d869043f8efd1e894a
Mason Mackaman <masondeanm@aol.com>
2022-07-23 03:28:34 -0400

    Add ssh keys for desktop-2019

--- systems/desktop-2019/info.nix
+++ systems/desktop-2019/info.nix
@@ -1,1 +1,5 @@
-{}
+{ ssh-keys =
+    { host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIfsgBKWSZWFad1C+S9WthcmBG/yyb4GTnGjHs4q3+6M";
+      mason = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ5zgfnd2zU9DV6wnhjtNr/VjuBo5jcm4NgO/ubMRb1n";
+    };
+}
