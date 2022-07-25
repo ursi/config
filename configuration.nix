6a306f5f4ae8eefa582ff8db4c25438f30be8d7b
Mason Mackaman <masondeanm@aol.com>
2022-07-25 10:55:26 -0400

    Add -L to rebuild alias

--- configuration.nix
+++ configuration.nix
@@ -61,7 +61,7 @@ with builtins;
                 { fui = "nix flake lock --update-input";
                   nix = "nix -L --allow-import-from-derivation";
                   nixpkgs-unstable = ''echo $(nix eval --impure --raw --expr '(fetchGit { url = "https://github.com/NixOS/nixpkgs"; ref = "nixpkgs-unstable"; }).rev')'';
-                  rebuild = "nixos-rebuild --use-remote-sudo";
+                  rebuild = "nixos-rebuild -L --use-remote-sudo";
                   repl = ''nix repl --arg pkgs '(builtins.getFlake "${./.}").inputs.nixpkgs.legacyPackages.x86_64-linux' ${./repl.nix}'';
                 };
 
