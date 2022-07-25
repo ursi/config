ececc7baf56faecc3e37190be93e2a4edb4fc14e
Mason Mackaman <masondeanm@aol.com>
2022-07-25 10:45:28 -0400

    Add ssh key for windows

--- systems/desktop-2019/info.nix
+++ systems/desktop-2019/info.nix
@@ -1,5 +1,7 @@
 { ssh-keys =
     { host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIfsgBKWSZWFad1C+S9WthcmBG/yyb4GTnGjHs4q3+6M";
       mason = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ5zgfnd2zU9DV6wnhjtNr/VjuBo5jcm4NgO/ubMRb1n";
+      windows = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAJqUEAXvjnCkafzzx3WUfc0xlffyt8xJOiSu6GE0Ipo Mason@desktop-2019";
     };
+
 }
