b80decb4258c2308a50693983ce26cb9c65b3ab6
Mason Mackaman <masondeanm@aol.com>
2022-09-16 22:12:14 -0400

    Add hp-envy ip

--- systems/hp-envy/info.nix
+++ systems/hp-envy/info.nix
@@ -1,4 +1,6 @@
-{ ssh-keys =
+{ ip = "216.73.176.71";
+
+  ssh-keys =
     { host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjLWvLvgY4VSV3rvihTjjLRtgHQIA50U43ALdl66IzO";
       mason = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICiC9a/2NVbF8Viqvq3kPdzIkQwAJUbK8btC54ovtMJa";
     };
