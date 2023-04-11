7827d0e5d90d292dcd371db8f1371c6055cfcd05
Mason Mackaman <masondeanm@aol.com>
2023-04-10 22:26:04 -0400

    Add CPU usage to i3status

--- systems/hp-envy/default.nix
+++ systems/hp-envy/default.nix
@@ -42,7 +42,12 @@ with builtins;
     system.stateVersion = "20.09";
 
     users.users.mason =
-      { im-home.i3status.status-bar = (import ../../i3status.nix).no-battery;
+      { im-home.i3status.status-bar =
+          [ { name = "cpu_usage";
+              config.format = "🖥️ %usage";
+            }
+          ]
+          ++ (import ../../i3status.nix).no-battery;
 
         remote-builder =
           { enable = true;
