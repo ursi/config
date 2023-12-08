40e4ad115319806fdc45729b632635b3ca47cdbb
Mason Mackaman <masondeanm@aol.com>
2023-12-07 23:37:46 -0500

    Alias :e to nvim

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,7 @@ with builtins;
             }
 
             { pkg = neovim;
+              aliases.":e" = "nvim";
               functions.note = ''nvim ~/notes/"''${1:-notes}".txt'';
             }
 
