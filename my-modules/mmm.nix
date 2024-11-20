587cbac2c052a8f2e6dec91a58e3a0398f52cc69
Mason Mackaman <masondeanm@aol.com>
2024-11-20 14:13:24 -0500

    Refactor silly expression
    - Remove redundant parens
    - Use l.id instead of a lambda

--- my-modules/mmm.nix
+++ my-modules/mmm.nix
@@ -39,6 +39,6 @@ set:
                   ([ "my-modules" ] ++ l.splitString "." nv.name ++ [ "hm" ])
                   (map (path: path ++ l.splitString "." nv.value) hm-paths)))
       (ms: ms ++ [ (moveAttr [ "my-modules" "hm" ] hm-paths) ])
-      (foldl' (acc: f: set: (acc (f (set)))) (_:_))
+      (foldl' (acc: f: set: acc (f set)) l.id)
       (f: f set)
     ]
