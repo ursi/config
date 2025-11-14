1381721a28addde6452313bb27e49ef80d02907d
Mason Mackaman <masondeanm@aol.com>
2025-11-13 21:36:24 -0500

    Switch to using an attrset

--- shell-scripts.nix
+++ shell-scripts.nix
@@ -1,44 +1,40 @@
 p:
-  let
-    inherit (p.lib) getExe;
-    inherit (p) writeShellScriptBin;
-  in
-  [ (writeShellScriptBin "bevel-delete"
-       ''
-       ${getExe p.sqlite} ~/.local/share/bevel/history.sqlite3 "DELETE FROM command WHERE text LIKE '$1'"
-       ${getExe p.sqlite} ~/.local/share/bevel/history.sqlite3 "DELETE FROM command WHERE text LIKE 'bevel-delete%'"
-       '')
+let inherit (p.lib) getExe; in
+p.lib.mapAttrsToList p.writeShellScriptBin
+  { bevel-delete =
+      ''
+      ${getExe p.sqlite} ~/.local/share/bevel/history.sqlite3 "DELETE FROM command WHERE text LIKE '$1'"
+      ${getExe p.sqlite} ~/.local/share/bevel/history.sqlite3 "DELETE FROM command WHERE text LIKE 'bevel-delete%'"
+      '';
 
-    (writeShellScriptBin "prefetch-nixpkgs"
-       ''
-       url=https://github.com/NixOS/nixpkgs/archive/$1.tar.gz
-       nix-prefetch-url $url --type sha256 --unpack 2> /dev/null | {
-         read hash
+    prefetch-nixpkgs =
+      ''
+      url=https://github.com/NixOS/nixpkgs/archive/$1.tar.gz
+      nix-prefetch-url $url --type sha256 --unpack 2> /dev/null | {
+        read hash
 
-         echo "\
-       (fetchTarball
-          { url = \"$url\";
-            sha256 = \"$hash\";
-          }
-       )"
-       }
-       ''
-    )
+        echo "\
+      (fetchTarball
+         { url = \"$url\";
+           sha256 = \"$hash\";
+         }
+      )"
+      }
+      '';
 
-    (writeShellScriptBin "unsymlink"
-       ''
-       set -e
-       tmp=$(mktemp)
-       cp -r "$1" $tmp
-       rm "$1"
-       cp -Lr $tmp "$1"
-       rm $tmp
+    unsymlink =
+      ''
+      set -e
+      tmp=$(mktemp)
+      cp -r "$1" $tmp
+      rm "$1"
+      cp -Lr $tmp "$1"
+      rm $tmp
 
-       if [ -d "$1" ]; then
-         chmod 755 "$1"
-       else
-         chmod 666 "$1"
-       fi
-       ''
-    )
-  ]
+      if [ -d "$1" ]; then
+        chmod 755 "$1"
+      else
+        chmod 666 "$1"
+      fi
+      '';
+  }
