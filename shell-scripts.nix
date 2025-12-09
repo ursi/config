5602b83722cdf1956dc2b6bf6e4ed83f57ebdb34
Mason Mackaman <masondeanm@aol.com>
2025-12-09 12:55:50 -0500

    Add additional characters to hc
    HTML only has &sup2; and &sup3;. I have extended that for all the
    digits and some useful symbols, as well as added "&subX;" versions.

--- shell-scripts.nix
+++ shell-scripts.nix
@@ -1,4 +1,5 @@
 p:
+with builtins;
 let inherit (p.lib) getExe; in
 p.lib.mapAttrsToList p.writeShellScriptBin
   { bevel-delete =
@@ -14,9 +15,37 @@ p.lib.mapAttrsToList p.writeShellScriptBin
             { url = "https://html.spec.whatwg.org/entities.json";
               hash = "sha256-10HYd6x3xBlMStUmtbShmu+N/kEauECkZokc27nzYuY=";
             };
+        additional =
+          toFile "additional-hc-characters.json"
+            (toJSON
+               { "&sup0;".characters = "⁰";
+                 "&sup4;".characters = "⁴";
+                 "&sup5;".characters = "⁵";
+                 "&sup6;".characters = "⁶";
+                 "&sup7;".characters = "⁷";
+                 "&sup8;".characters = "⁸";
+                 "&sup9;".characters = "⁹";
+                 "&supn;".characters = "ⁿ";
+                 "&sup+;".characters = "⁺";
+                 "&sup-;".characters = "⁻";
+                 "&sub0;".characters = "₀";
+                 "&sub1;".characters = "₁";
+                 "&sub2;".characters = "₂";
+                 "&sub3;".characters = "₃";
+                 "&sub4;".characters = "₄";
+                 "&sub5;".characters = "₅";
+                 "&sub6;".characters = "₆";
+                 "&sub7;".characters = "₇";
+                 "&sub8;".characters = "₈";
+                 "&sub9;".characters = "₉";
+                 "&subn;".characters = "ₙ";
+                 "&sub+;".characters = "₊";
+                 "&sub-;".characters = "₋";
+               });
       in
       ''
-      ${getExe p.jq} -r '."&'$1';".characters' ${character-references} |
+      ${getExe p.jq} -rs '.[0] * .[1] | ."&'$1';".characters' \
+        ${character-references} ${additional} |
         tr -d '\n' |
         ${getExe p.xclip} -selection clipboard
       '';
