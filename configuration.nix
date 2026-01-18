1deb5f19e45471196b9c7ab21054ba49fc48d19c
Mason Mackaman <masondeanm@aol.com>
2026-01-17 20:55:45 -0500

    Stop using aliased command
    No point, this is easier to find the documentation for

--- configuration.nix
+++ configuration.nix
@@ -204,7 +204,7 @@ with builtins;
 
                functions =
                  { e-inkn = ''${e-ink} new -n "$1" -s "$1"'';
-                   tmuxn = ''tmux new -n "$1" -s "$1"'';
+                   tmuxn = ''tmux new-session -n "$1" -s "$1"'';
                  };
              }
             )
